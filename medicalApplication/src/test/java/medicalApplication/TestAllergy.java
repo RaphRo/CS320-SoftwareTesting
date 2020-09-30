2743d0c02e6d9e0e5068aab51c5aedea7de324c6
Rafro <Rafro@DESKTOP-GN11THH>
2020-09-30 14:06:54 -0500

    Updated Allergy Tests

--- medicalApplication/src/test/java/medicalApplication/TestAllergy.java
+++ medicalApplication/src/test/java/medicalApplication/TestAllergy.java
@@ -3,13 +3,14 @@ package medicalApplication;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import medical.com.medicalApplication.model.Allergey;
 
 public class TestAllergy {
 	private  Allergey allergey;
+	
+	private String name = "WURMS";
 
 	@Before
 	public void setUp() throws Exception {
@@ -18,8 +19,25 @@ public class TestAllergy {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructor() {
+		String expectedName = name.toString();
+		String actualName = allergey.getName();
+		assertTrue(expectedName.equals(actualName));
+	}
+
+	@Test
+	public void testSetName() {
+		String expectedValue = "NewWURMS";
+		allergey.setName(expectedValue);
+		String actualValue = allergey.getName();
+		assertTrue(expectedValue.equals(actualValue));
+	}
+	
+	@Test
+	public void testToString() {
+		String expectedValue = "Allergy " + name;
+		String actualValue = allergey.toString();
+		assertTrue(expectedValue.equals(actualValue)) ;
 	}
 
 }
