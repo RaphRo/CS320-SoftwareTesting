400c0919a725a1d47ca4050d12f4c683a6770d3a
Rafro <Rafro@DESKTOP-GN11THH>
2020-10-02 16:52:20 -0500

    Tests Patients added Junit library

---
+++ medicalApplication/src/test/java/medicalApplication/TestPatient.java
@@ -0,0 +1,22 @@
+package medicalApplication;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import medical.com.medicalApplication.model.Patient;
+
+public class TestPatient {
+	Patient patient;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
